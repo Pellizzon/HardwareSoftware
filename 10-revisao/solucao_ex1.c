ea025284c159ec4d0d605548ea60a78b3c3e7e53
Pellizzon <matheuspellizzon@hotmail.com>
2020-04-02 17:17:33 -0300

    aula 10

--- 10-revisao/solucao_ex1.c
+++ 10-revisao/solucao_ex1.c
@@ -1,4 +1,15 @@
 
-extern <tipo> var1; //TODO: complete com o tipo correto
+extern long var1; //TODO: complete com o tipo correto
 
-//TODO: implemente a função solucao_ex1 abaixo 
+//TODO: implemente a função solucao_ex1 abaixo
+
+int solucao_ex1()
+{
+    int var = 0;
+    while (var1 > 0)
+    {
+        faz_algo();
+        var++;
+    }
+    return var;
+}
--- 10-revisao/solucao_ex3.c
+++ 10-revisao/solucao_ex3.c
@@ -1,5 +1,9 @@
 // TODO: substituir tipo pelo tipo das globais
-extern <tipo> var1, var2;
+extern int var1, var2;
 
 // TODO: implementar solucao_ex3 abaixo
 
+void solucao_ex3(int *a)
+{
+    *a = var1 * 5 + var2 * 10;
+}

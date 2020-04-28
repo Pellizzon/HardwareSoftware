fb3f24b927573f5cf52194934778ff7f7a45e38a
Pellizzon <matheuspellizzon@hotmail.com>
2020-04-28 14:32:47 -0300

    13

--- 13-TAD/point2d.c
+++ 13-TAD/point2d.c
@@ -1,39 +1,55 @@
 #include "point2d.h"
 #include <stdlib.h>
 
-struct _p {
+struct _p
+{
     double x, y;
 };
 
 typedef struct _p Point2D;
 
-
-Point2D *point2D_new(double x, double y) {
-    return NULL;
+Point2D *point2D_new(double x, double y)
+{
+    Point2D *p = malloc(sizeof(Point2D));
+    p->x = x;
+    p->y = y;
+    return p;
 }
 
-
-void point2D_destroy(Point2D *p) {
-    
+void point2D_destroy(Point2D *p)
+{
+    free(p);
+    p = NULL;
 }
 
-double point2D_get_x(Point2D *p) {
-    return 0;
+double point2D_get_x(Point2D *p)
+{
+    return p->x;
 }
 
-double point2D_get_y(Point2D *p) {
-    return 0;
+double point2D_get_y(Point2D *p)
+{
+    return p->y;
 }
 
-Point2D *point2D_add(Point2D *p1, Point2D *p2) {
-    return NULL;
+Point2D *point2D_add(Point2D *p1, Point2D *p2)
+{
+    Point2D *c = point2D_new(0, 0);
+    c->x = p1->x + p2->x;
+    c->y = p1->y + p2->y;
+    return c;
 }
 
-double point2D_theta(Point2D *p1, Point2D *p2) {
-    return 0;
+double point2D_theta(Point2D *p1, Point2D *p2)
+{
+
+    return (p1->y - p2->y) / (p1->x - p2->x);
 }
 
-Point2D *point2D_scale(Point2D *p, double s) {
-    return NULL;
+Point2D *point2D_scale(Point2D *p, double s)
+{
+    Point2D *c = point2D_new(0, 0);
+    c->x = p->x * s;
+    c->y = p->y * s;
+    return c;
 }
-
--- 13-TAD/vec_int.c
+++ 13-TAD/vec_int.c
@@ -2,33 +2,90 @@
 
 #include "vec_int.h"
 
-typedef struct _vec_int {
+typedef struct _vec_int
+{
     int *data;
     int size;
     int capacity;
 } vec_int;
 
+vec_int *vec_int_create()
+{
 
-vec_int *vec_int_create() {
-    return NULL;
+    vec_int *v = malloc(sizeof(vec_int));
+    v->capacity = 2;
+    v->data = malloc(2 * sizeof(int));
+    v->size = 0;
+
+    return v;
 }
 
-void vec_int_destroy(vec_int **_v) {
-
+void vec_int_destroy(vec_int **_v)
+{
+    vec_int *v = *_v;
+    free(v->data);
+    free(v);
+    *_v = NULL;
 }
 
-int vec_int_size(vec_int *v) {
+int vec_int_size(vec_int *v)
+{
+    return v->size;
+}
+
+int vec_int_at(vec_int *v, int pos, int *val)
+{
+    if (pos < v->size)
+    {
+        *val = v->data[pos];
+        return 1;
+    }
     return 0;
 }
 
-int vec_int_at(vec_int *v, int pos, int *val) {
-    return 0;
+int vec_int_insert(vec_int *v, int pos, int val)
+{
+    if (pos < 0 || pos >= v->capacity)
+    {
+        return 0;
+    }
+
+    v->size++;
+
+    if (v->size == v->capacity)
+    {
+        v->capacity *= 2;
+        v->data = realloc(v->data, sizeof(int) * v->capacity);
+    }
+
+    for (int i = v->size - 1; i > pos; i--)
+    {
+        v->data[i] = v->data[i - 1];
+    }
+    v->data[pos] = val;
+
+    return 1;
 }
 
-int vec_int_insert(vec_int *v, int pos, int val) {
-    return 0;
-}
+int vec_int_remove(vec_int *v, int pos)
+{
+    if (pos < 0 || pos >= v->size)
+    {
+        return 0;
+    }
 
-int vec_int_remove(vec_int *v, int pos) {
-    return 0;    
+    if (v->size == v->capacity / 4)
+    {
+        v->capacity /= 2;
+        v->data = realloc(v->data, sizeof(int) * v->capacity);
+    }
+
+    for (int i = pos; i < v->size - 1; i++)
+    {
+        v->data[i] = v->data[i + 1];
+    }
+
+    v->size--;
+
+    return 1;
 }
